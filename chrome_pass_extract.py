05abe79a7a1efd31efdbe998006fe8f6fe849afd
Raphael Viana <raphaelnapi@gmail.com>
2025-10-06 21:28:15 -0300

    Update chrome_pass_extract.py
    Code optimization

--- chrome_pass_extract.py
+++ chrome_pass_extract.py
@@ -26,25 +26,23 @@ db_con = sqlite3.connect(login_data_file_path)
 db_cur = db_con.cursor()
 db_cur.execute("SELECT action_url, username_value, password_value FROM logins")
 
-arq = open("chrome_password_extrated.csv", "w")
-arq.write(f"Action URL,Username,Password\n")
-
-for index, data in enumerate(db_cur.fetchall()):
-    url = data[0]
-    username = data[1]
-    password_value = data[2]
-
-    # Dados criptografados que você quer descriptografar
-    password_value = password_value[3:] #descarta v10 que indica parâmetros de criptografia do Chrome
-    iv = password_value[:12] #primeiros 12 bytes são o IV
-    encrypted_data = password_value[12:] #demais bytes são o CT
-
-    # Descriptografar
-    cipher = AES.new(key, AES.MODE_GCM, iv)
-    decrypted_data = cipher.decrypt(encrypted_data)
-    password = decrypted_data[:-16].decode()
-    tag = decrypted_data[16:]
-    print(f"Action URL: {url}\nUsername: {username}\nPassword: {password}\n\n")
-    arq.write(f"{url},{username},{password}\n")
-
-arq.close()
+with open("chrome_password_extrated.csv", "w") as arq:
+    arq.write(f"Action URL,Username,Password\n")
+    
+    for index, data in enumerate(db_cur.fetchall()):
+        url = data[0]
+        username = data[1]
+        password_value = data[2]
+    
+        # Dados criptografados que você quer descriptografar
+        password_value = password_value[3:] #descarta v10 que indica parâmetros de criptografia do Chrome
+        iv = password_value[:12] #primeiros 12 bytes são o IV
+        encrypted_data = password_value[12:] #demais bytes são o CT
+    
+        # Descriptografar
+        cipher = AES.new(key, AES.MODE_GCM, iv)
+        decrypted_data = cipher.decrypt(encrypted_data)
+        password = decrypted_data[:-16].decode()
+        tag = decrypted_data[16:]
+        print(f"Action URL: {url}\nUsername: {username}\nPassword: {password}\n\n")
+        arq.write(f"{url},{username},{password}\n")    
